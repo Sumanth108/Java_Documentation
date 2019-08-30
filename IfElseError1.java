3ce81b391a3e885d14f340f3a55d0a8ead60f262
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2019-08-30 12:21:02 +0530

    Create IfElseError1.java

---
+++ IfElseError1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+  class IfElseError1
+  {
+    public static void main(String args[])
+     { int x = 0;
+       if(x)    //HERE 0 IS NOT EQUAL TO FALSE AND VICE VERSA {Will give incompatable type ERROR)
+          System.out.println("Hellp");
+       else
+          System.out.println("bakwas");       
+       
+       
+     } 
+  } 
